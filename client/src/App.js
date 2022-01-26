4bf9f079e883be46e42e43685db509a824819953
Anthony <tony.hongq@gmail.com>
2022-01-26 01:07:47 -0800

    add post comment sign_in sign_up

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
 import Header from "./components/Header";
 import Nav from "./components/Nav";
 import Main from "./components/MainPosts";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Post from "./components/Post";
 
 function App() {
   return (
     <>
-      <Nav />
+      {/* <Nav />
       <Header />
       <Main />
-      
+       */}
+      {/* <Login />
+      <Signup /> */}
+      <Nav />
+      <Post />
     </>
+    
     
   );
 }
--- client/src/components/PostThumbnail.js
+++ client/src/components/PostThumbnail.js
@@ -5,10 +5,10 @@ function PostThumbnail() {
 
     <div className="col-6 mt-4 ">
         <div className="card shadow">
-            <img className="card-img-top object-fit-cover" src="https://images.unsplash.com/reserve/91JuTaUSKaMh2yjB1C4A_IMG_9284.jpg?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" alt="Card image cap"/>
+            <img className="card-img-top object-fit-cover" src="https://images.unsplash.com/reserve/91JuTaUSKaMh2yjB1C4A_IMG_9284.jpg?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" alt="post"/>
             <div className="card-body">
                 <div className="d-flex align-items-center">
-                    <img className="rounded-circle img-user" src="https://randomuser.me/api/portraits/men/36.jpg"/>
+                    <img className="rounded-circle img-user" src="https://randomuser.me/api/portraits/men/36.jpg" alt="user"/>
                     <p className="m-0 px-2">John Doe</p>
                 </div>
                 
